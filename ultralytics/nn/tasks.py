f79af70e5eb5d3bfa3ed5541886dce8fea0a891b
KorkanaRahul <korkanarahul@gmail.com>
2025-02-04 16:49:32 +0530

    Update tasks.py
    DFL Global decl
    
    Co-Authored-By: KONDEPUDI VENKATA DURGA  <75824905+venkatdurga@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -68,6 +68,8 @@ from ultralytics.nn.modules import (
     WorldDetect,
     v10Detect,
 )
+globals()["DFL"] = DFL
+
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
 from ultralytics.utils.loss import (
